36d8aef3aa51518d76b2f13adc7d9ab49b77af12
svelshi19 <109823002+svelshi19@users.noreply.github.com>
2022-07-29 16:00:15 -0400

    Update TreeNode.hpp

--- BinaryTreeRightSideView/inc/TreeNode.hpp
+++ BinaryTreeRightSideView/inc/TreeNode.hpp
@@ -1,11 +1,11 @@
 #include <vector>
 
 class TreeNode {
-public:
-  int val;
-  TreeNode *left;
-  TreeNode *right;
-  TreeNode();
-  TreeNode(int x);
-  TreeNode(int x, TreeNode *left, TreeNode *right);
+    int val;
+    TreeNode *left;
+    TreeNode *right;
+  public:
+    TreeNode();
+    TreeNode(int x);
+    TreeNode(int x, TreeNode *left, TreeNode *right);
 };
