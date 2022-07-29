76750e49cb25686144e64fd699ac9cfc361539a9
svelshi19 <109823002+svelshi19@users.noreply.github.com>
2022-07-29 16:03:36 -0400

    Update main.cpp

--- Palindrome/src/main.cpp
+++ Palindrome/src/main.cpp
@@ -1,3 +1,5 @@
+// This file contains test cases to test the palindrome function.
+
 #include "Palindrome.hpp"
 #include "TestUtils.hpp"
 #include <iostream>
@@ -6,16 +8,39 @@ int main() {
   Palindrome palindrome;
 
   // Test cases
+  /* Test Case 1:
+     Input: 121, output: True
+  */
   EXPECT_TRUE(palindrome.isPalindrome(121), "Test_121");
   std::cout << std::endl;
+  
+  /* Test Case 2:
+     Input: -121, output: False
+  */
   EXPECT_FALSE(palindrome.isPalindrome(-121), "Test_Negative121");
   std::cout << std::endl;
+  
+  /* Test Case 3:
+     Input: 1234567899, output: False
+  */
   EXPECT_FALSE(palindrome.isPalindrome(1234567899), "Test_1234567899");
   std::cout << std::endl;
+  
+  /* Test Case 4:
+     Input: 99999, output: True
+  */
   EXPECT_TRUE(palindrome.isPalindrome(99999), "Test_99999");
   std::cout << std::endl;
+  
+  /* Test Case 5:
+     Input: 0, output: True
+  */
   EXPECT_TRUE(palindrome.isPalindrome(0), "Test_0");
   std::cout << std::endl;
+  
+  /* Test Case 6:
+     Input: 1234, output: False
+  */
   EXPECT_FALSE(palindrome.isPalindrome(1234), "Test_1234");
   std::cout << std::endl;
 }
